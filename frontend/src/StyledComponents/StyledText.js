6e0934391826050b31ef3b1d4600ba4dc2166dd3
siyanet <siyangetahunw@gmail.com>
2024-06-13 15:41:45 +0300

    styledText added

---
+++ frontend/src/StyledComponents/StyledText.js
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled/macro";
+
+export const Heading = styled.h1`
+font-family: ${props => props.theme.fonts.heading};
+font-size: ${props => `${props.theme.fontSizes[3]}px`};
+color: ${props => props.theme.colors.white};
+font-weight:${props => props.theme.fontWeights.heading};
+line-height: ${props => `${props.theme.lineHeights.heading}px`};
+`;
+export const P= styled.p`
+font-family: ${props => props.theme.fonts.body};
+font-size: ${props => `${props.theme.fontSizes[props.size]}px`};
+color: ${props => props.theme.colors.white};
+font-weight:${props => props.theme.fontWeights[props.weight] || props.theme.fontWeights.body};
+line-height: ${props => `${props.theme.lineHeights.body}px`};
+`;
+export const Heading3 = styled.h3`
+font-family: ${props => props.theme.fonts.bold};
+font-size: ${props => `${props.theme.fontSizes[2]}px`};
+color: ${props => props.theme.colors.white};
+font-weight:${props => props.theme.fontWeights.bold};
+line-height: ${props => `${props.theme.lineHeights.heading}px`};
+`;
