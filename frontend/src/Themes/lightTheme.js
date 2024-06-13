3a028b55f2413c0915ae7c7e764aa18680f3db74
siyanet <siyangetahunw@gmail.com>
2024-06-13 14:15:36 +0300

    adding styled componenets for table'

---
+++ frontend/src/Themes/lightTheme.js
@@ -0,0 +1,34 @@
+const lightTheme = {
+    colors: {
+      primary: '#006100',
+      secondary: '#059e08',
+      tertiary: '#c1e167',
+      white: '#fff',
+      gray: '#303030',
+      black: '#033600',
+      
+    },
+    space: [0, 4, 8, 16, 32, 64],
+    fonts: {
+      heading: 'Roboto Mono, monospace',
+      body: 'Roboto Slab, serif',
+      special: 'Lora, sans-serif',
+    },
+    fontSizes: [15,21,32,40],
+    fontWeights: {
+      body: 300,
+      heading: 500,
+      bold: 400,
+    },
+    lineHeights: {
+      body: 1.5,
+      heading: 1.25,
+    },
+    radii: {
+      default: 4,
+      circle: 99999,
+    },
+  };
+  
+  export default lightTheme;
+  
